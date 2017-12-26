a0bd8d8c20d427ef9c5830064c46af9e2ea85fa6
idabagusangga <angga.idabagus@gmail.com>
2017-12-26 15:38:59 +0700

    dummy homepage with cards and login

---
+++ livefree/src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello">
+
+          <div class="jumbotron">
+              <h1 class="text-warning">Find Your Neighborhood</h1>
+              <p class="text-warning">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+              <hr class="my-4">
+              <p class="text-warning">It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              </p>
+          </div>
+          
+          
+          <div class="">
+              <login/>
+          </div>
+          
+          <br>
+          
+         <div class="row">
+             <!-- looped later -->
+              <rumah/> 
+         </div>
+            
+          
+      
+  </div>
+</template>
+
+<script>
+import rumah from './rumah'
+import login from './login'
+export default {
+
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components : {
+      rumah : rumah,
+      login : login
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.jumbotron {
+    background-image: url(../assets/pablo-vargas-17775.jpg);
+    background-size: 100%;
+    background-repeat: no-repeat;
+    
+}
+
+.display-3{
+    color: gray;
+}
+</style>
---
+++ livefree/src/components/login.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="">
+      <button type="button" class="btn btn-primary btn-lg btn-block">Login to view Price</button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
